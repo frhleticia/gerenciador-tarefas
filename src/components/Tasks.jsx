da28e14b75cf930efcd15826cfc83ee8eff1d050
leticia.silva <frhleticia@gmail.com>
2026-04-27 02:20:05 -0300

    Update Tasks component again

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRight, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRight, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
